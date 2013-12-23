b675b4a7137894cf1e354f157c7383fd5146c9d9
Evan Chen <hello@evan.lc>
2013-12-23 00:42:00 -0800

    initial commit p2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Choossweetstuff::Application.config.secret_key_base = '33419bc45ebe4a38f0014e29ee9907d81d17851c8a47f3af5db57075d3074ff7d0050e7958b3a473e20a4340c952ad96278ad21c11073ffcf0fc3a7674f877d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Choossweetstuff::Application.config.session_store :cookie_store, key: '_choossweetstuff_session'
